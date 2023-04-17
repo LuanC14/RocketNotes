aeda2d4d37ca9b74cf9b54862db12446ec8cd5f4
LuanC14 <luanchr14@gmail.com>
2023-04-17 12:37:17 -0300

    Add: base_URL server deployed

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,5 +1,6 @@
- import axios from 'axios'
- 
- export const api = axios.create({
-    baseURL: "http://localhost:3333"
- })
+import axios from 'axios'
+
+export const api = axios.create({
+   //  baseURL: "http://localhost:3333",
+   baseURL: "https://back-end-rocket-notes.onrender.com"
+})
